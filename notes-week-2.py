42fd7b78bf33a7d397b3be9e70a8af0483804031
Kelsey Crow <kelseybcrow@gmail.com>
2026-09-02 08:46:49 -0600

    wednesday a.m.

---
+++ notes-week-2.py
@@ -0,0 +1,5 @@
+|0| = 0
+
+|x| = -number  ~~  no solution
+
+#need all ALEKS problems unlocked
---
+++ week-2.py
@@ -0,0 +1,129 @@
+
+'''
+Algebra week two
+'''
+
+'''
+Example problem notation:
+'''
+
+3(x - 2) + 4  =  19 
+
+#step one: distribute the 3 to the (x-2)
+3x - 6 + 4  =  19
+
+#step two: combine constants
+#-6 + 4 =  -2
+3x - 2  =  19
+
+#step three: add 2 to both sides
+(3x - 2) + 2  =  (19) + 2
+#-2 + 2 = 0; 19 + 2 = 21
+(3x)  =  (21)
+
+#step four: divide both sides by 3
+(3x) / 3  =  (21) / 3
+#3 / 3 = 1; 21 / 3 = 7
+x  =  7 
+
+'''
+problem 1
+'''
+
+f(x) = -3x^3 - 2
+g(x) = 3x + 2
+
+Find f(-4) and g(-3)
+
+#step one: substitute -4 for x in f(x)
+f(-4) = -3(-4)^3 - 2
+      = -3(-64) - 2
+      = 192 - 2
+      = 190
+
+#step two: substitute -3 for x in g(x)
+g(-3) = 3(-3) + 2
+      = -9 + 2
+      = -7
+
+#step three: final answers
+f(-4) = 190
+g(-3) = -7
+ 
+'''
+Solving a linear equation with several occurrences of the variable: Variables on both sides and distribution
+'''
+
+7x - 15  =  3(x - 1)
+
+#step one: distribute the 3 to the (x-1)
+7x - 15  =  3x - 3
+
+#step two: subtract 3x from both sides
+(7x - 15) - 3x  =  (3x - 3) - 3x
+#7x - 3x = 4x; 3x - 3x = 0
+4x - 15  =  -3
+
+#step three: add 15 to both sides
+(4x - 15) + 15  =  (-3) + 15
+#-15 + 15 = 0; -3 + 15 = 12
+4x  =  12
+
+#step four: divide both sides by 4
+(4x / 4)  =  (12 / 4)
+#4 / 4 = 1;  12 / 4 = 3
+1x  =  3
+
+'''
+Graph the following functions
+'''
+
+f(x) = x
+f(x) = (x^2)
+f(x) = |x|
+f(x) = (x^3)
+
+f(x) = x
+#step one: create a table of values for both: x and f(x)
+x  | f(x)    
+-2 | -2
+-1 | -1
+ 0 |  0
+ 1 |  1
+ 2 |  2
+
+'''
+graph is done in desmos; it's a straight line through the (0, 0) origin with a slope of rise: 1 / run: 1
+'''
+f(x) = (x^2)
+#step one: create a table of values for both: x and f(x)
+#f(x) = each x value is squared
+x  | f(x)   
+-2 |  4
+-1 |  1
+ 0 |  0
+ 1 |  1
+ 2 |  4
+
+'''
+graph is done in desmos; it's a skinny parabola opening **upwards** with the vertex (center) at the (0, 0) origin
+why is the parabola skinny? because the coefficient of x^2 is 1, which is a small number; if it were a larger number, the parabola would be wider
+so if the graph was f(x) = 10x^2, the parabola would be fatter than f(x) = x^2 
+'''
+
+f(x) = |x|
+
+#step one: create a table of values for both: x and f(x)
+x  | f(x)
+-2 |  2
+-1 |  1
+ 0 |  0
+ 1 |  1
+ 2 |  2
+
+'''
+graph is done in desmos; it's a v-shaped graph with the vertex (center) at the (0, 0) origin
+why is it v-shaped? because the absolute value of a number is always positive, so the graph will always be above the x-axis
+so the line will be on the left if the y-axis is negative, and on the right if the y-axis is positive
+but it will never be on the bottom since the x-axis will always be positive
+'''
