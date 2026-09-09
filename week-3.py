ef151c3d011ae54126b4a385a55c29bb5b7873f4
Kelsey Crow <kelseybcrow@gmail.com>
2026-09-08 19:50:47 -0600

    tuesday evening

---
+++ week-3.py
@@ -0,0 +1,167 @@
+'''
+simplify the square root of a whole number < 100
+'''
+√‾‾32
+
+#find the largest perfect square factor ---> 16
+32 = 16 * 2
+
+#rewrite square root of 32 as square root of (16 * 2)
+
+√‾‾32  =  √‾‾(16 * 2)  
+
+#separate the square roots
+√‾‾32  =  √‾‾16 * √‾‾2  
+
+#simplify √‾‾16
+√‾‾32  =  4 * √‾‾2
+4 * √‾‾2
+
+
+'''
+rewrite without an exponent
+'''
+(3 / 8)⁻²
+'''
+
+negative exponent rule
+(a / b)⁻ⁿ = (b / a)ⁿ = (b² / a²)
+'''
+
+#swap the numerator and denominator and change the negative exponent to be positive 
+(3 / 8)⁻² = 
+(8 / 3)²
+
+#distribute the exponent to both the numerator and denominator
+(8 / 3)² = 
+(8² / 3²) = 
+(64 / 9) 
+
+
+'''
+rewrite without an exponent
+'''
+(4 / 3)⁻¹
+'''
+
+negative exponent rule
+(a / b)⁻ⁿ = (b / a)ⁿ = (b² / a²)
+'''
+
+#swap the numerator and denominator and change the negative exponent to be positive 
+(4 / 3)⁻¹ = 
+(3 / 4)¹
+
+#distribute the exponent to both the numerator and denominator
+(3 / 4)² = 
+
+
+'''
+rewrite without an exponent
+'''
+(4 / 5)⁻³
+'''
+
+negative exponent rule
+(a / b)⁻ⁿ = (b / a)ⁿ = (b² / a²)
+'''
+
+#swap the numerator and denominator and change the negative exponent to be positive 
+(5 / 4)³ =
+
+#distribute the exponent to both the numerator and denominator
+((5)³ / (4)³) = 
+(125 / 64) 
+
+#it doesn't simplify further than this because 125 and 64 have no common factors
+
+
+'''
+rewrite without an exponent
+'''
+(7 / 2)⁻²
+'''
+
+negative exponent rule
+(a / b)⁻ⁿ = (b / a)ⁿ = (b² / a²)
+'''
+
+#swap the numerator and denominator and change the negative exponent to be positive 
+(2 / 7)² 
+
+#distribute the exponent to both the numerator and denominator
+((2)² / (7)²) = 
+(4 / 49)
+
+#it doesn't simplify further than this because 4 and 49 have no common factors
+
+
+'''
+evaluate
+'''
+- (6)⁰
+(- (1 / 7))⁰
+
+'''
+power of 0 rule: anything nonzero raised to the power of 0 becomes 1
+(a)⁰ = 1
+'''
+
+- (6)⁰ =
+- (1) =
+-1
+
+(- (1 / 7))⁰ = 
+1
+
+#everything inside the parentheses (including the negative sign) is raised to the power of 0, so the whole thing becomes 1
+
+
+'''
+evaluate
+'''
+- (5 / 7)⁰
+8⁰
+
+'''
+power of 0 rule: anything nonzero raised to the power of 0 becomes 1
+(a)⁰ = 1
+'''
+
+- (5 / 7)⁰ =
+- (1) =
+-1
+
+8⁰ = 
+1
+
+#everything inside the parentheses (including the negative sign) is raised to the power of 0, so the whole thing becomes 1
+
+
+'''
+evaluate
+'''
+4⁰
+(-3) (2 / 5)⁰
+
+'''
+power of 0 rule: anything nonzero raised to the power of 0 becomes 1
+(a)⁰ = 1
+'''
+
+4⁰ = 
+1
+
+(-3) (2 / 5)⁰ =
+(-3) (1) =
+-3
+
+# everything inside the parentheses (including the negative sign) is raised to the power of 0, so the whole thing becomes 1
+
+
+
+
+
+
+
+
