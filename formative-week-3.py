fcf6a876f1ba701b8ad686cac6883a402b881805
Kelsey Crow <kelseybcrow@gmail.com>
2026-09-10 16:22:52 -0600

    thursday afternoon

---
+++ formative-week-3.py
@@ -0,0 +1,47 @@
+Kelsey Crow
+MATH-143
+9-9-26
+
+'''
+simplify
+'''
+√‾‾96
+
+#the first 5 perfect squares are 1, 4, 9, 16, and 25
+#96 / 16 = 6
+
+√‾‾96 = 
+√‾‾16  *  √‾‾6 =
+4 * √‾‾6
+
+'''
+what are the leading coefficient and degree of the polynomial? 
+'''
+-(6) + (23w)⁸ + (2w)
+
+'''
+-(6) and 8th power?
+'''
+
+'''
+evaluate
+'''
+-(6)⁰ =
+-(1)
+
+(-(3 / 5))⁰ =
+1
+
+-(-6)³ = 
+
+#solving the -6 cubed first
+(-6) * (-6) = 36 * (-6) = (-216) =
+
+#adding the leading negative sign back in
+-(-216)
+
+#double-negatives make a positive
+216
+
+
+
--- week-3.py
+++ week-3.py
@@ -1,5 +1,5 @@
 '''
-simplify the square root of a whole number < 100
+simplifying the square root of a whole number < 100
 '''
 √‾‾32
 
@@ -19,7 +19,7 @@ simplify the square root of a whole number < 100
 
 
 '''
-rewrite without an exponent
+applying the negative exponent rule to a fraction
 '''
 (3 / 8)⁻²
 '''
@@ -39,7 +39,7 @@ negative exponent rule
 
 
 '''
-rewrite without an exponent
+applying the negative exponent rule to a fraction
 '''
 (4 / 3)⁻¹
 '''
@@ -57,7 +57,7 @@ negative exponent rule
 
 
 '''
-rewrite without an exponent
+applying the negative exponent rule to a fraction
 '''
 (4 / 5)⁻³
 '''
@@ -77,11 +77,11 @@ negative exponent rule
 
 
 '''
-rewrite without an exponent
+applying the negative exponent rule to a fraction
 '''
 (7 / 2)⁻²
-'''
 
+'''
 negative exponent rule
 (a / b)⁻ⁿ = (b / a)ⁿ = (b² / a²)
 '''
@@ -97,7 +97,7 @@ negative exponent rule
 
 
 '''
-evaluate
+applying the power of 0 rule to various expressions
 '''
 - (6)⁰
 (- (1 / 7))⁰
@@ -118,7 +118,7 @@ power of 0 rule: anything nonzero raised to the power of 0 becomes 1
 
 
 '''
-evaluate
+applying the power of 0 rule to various expressions
 '''
 - (5 / 7)⁰
 8⁰
@@ -139,7 +139,7 @@ power of 0 rule: anything nonzero raised to the power of 0 becomes 1
 
 
 '''
-evaluate
+applying the power of 0 rule to various expressions
 '''
 4⁰
 (-3) (2 / 5)⁰
@@ -159,9 +159,169 @@ power of 0 rule: anything nonzero raised to the power of 0 becomes 1
 # everything inside the parentheses (including the negative sign) is raised to the power of 0, so the whole thing becomes 1
 
 
+'''
+simplifying expressions with negative exponents
+'''
+(-2)⁻³
+
+'''
+negative exponent rule
+(a / b)⁻ⁿ = (b / a)ⁿ = (b² / a²)
+'''
+
+#swap the numerator and denominator and change the negative exponent to be positive 
+#keep the integer base negative since we only got rid of the negative exponent from using the 'negative exponent rule'
+(-2)⁻³ =
+1 / (-2)³ =
+
+1 / (-8) = 
+-(1 / 8)
 
 
+'''
+simplifying expressions with negative exponents
+'''
+(-5)⁻²
+
+'''
+negative exponent rule
+(a / b)⁻ⁿ = (b / a)ⁿ = (b² / a²)
+'''
+
+#swap the numerator and denominator and change the negative exponent to be positive 
+#keep the integer base negative since we only got rid of the negative exponent from using the 'negative exponent rule' 
+(-5)⁻² =
+1 / (-5)² =
+
+1 / 25
+
+#it doesn't simplify further than this because 1 and 25 have no common factors
 
 
+'''
+simplifying expressions with negative exponents
+'''
+(-3)⁻²
+
+'''
+negative exponent rule
+(a / b)⁻ⁿ = (b / a)ⁿ = (b² / a²)
+'''
+
+#swap the numerator and denominator and change the negative exponent to be positive 
+#keep the integer base negative since we only got rid of the negative exponent from using the 'negative exponent rule' 
+(-3)⁻³ =
+1 / (-3)³ =
 
 
+1 / (-27) =
+-(1 / 27)
+#it doesn't simplify further than this because 1 and 27 have no common factors
+
+
+'''
+simplifying expressions with negative exponents
+'''
+(-8)⁻²
+
+'''
+negative exponent rule
+(a / b)⁻ⁿ = (b / a)ⁿ = (b² / a²)
+'''
+
+#swap the numerator and denominator and change the negative exponent to be positive 
+#keep the integer base negative since we only got rid of the negative exponent from using the 'negative exponent rule' 
+(-8)⁻² =
+1 / (-8)² =
+1 / 64
+    
+#it doesn't simplify further than this because 1 and 64 have no common factors
+
+
+'''
+simplifying trinomials with negative coefficients, and variables raised to a power
+'''
+((-4) * (y)² * (x))³ =
+
+#distribute the exponent to each factor inside the parentheses ➜ ➜
+(-4)³ * ((y)²)³ * (x)³ =
+
+#multiply doubled-up exponents together, so ((y)²)³ becomes (y)⁶ ➜ ➜
+(-64) * (y)⁶ * (x)³
+
+#this can't be simplified further because there are no like terms or common factors
+
+
+'''
+simplifying trinomials with negative coefficients, and variables raised to a power
+'''
+((-7) * (y) * (x)³)² =
+
+#distribute the exponent to each factor inside the parentheses ➜ ➜
+(-7)² * (y)² * ((x)³)² =
+
+#multiply doubled-up exponents together ➜ ➜
+(49) * (y)² * (x)⁶ =
+49 y² x⁶ 
+
+#this can't be simplified further because there are no like terms or common factors
+
+
+'''
+simplifying trinomials with negative coefficients, and variables raised to a power
+'''
+(2 * (w)² * (v)³)⁵ =
+
+#distribute the exponent to each factor inside the parentheses ➜ ➜
+(2)⁵ * ((w)²)⁵ * ((v)³)⁵ =
+
+#multiply doubled-up exponents together ➜ ➜
+(32) * ((w)¹⁰) * ((v)¹⁵) =
+32 w¹⁰ v¹⁵
+
+#this can't be simplified further because there are no like terms or common factors
+
+
+'''
+simplifying trinomials with negative coefficients, and variables raised to a power
+'''
+((-5) * (u)² * (w))³
+
+#distribute the exponent to each factor inside the parentheses ➜ ➜
+(-5)³ * ((u)²)³ * (w)³ =
+
+#multiply doubled-up exponents together ➜ ➜
+(-125) * (u)⁶ * (w)³ =
+-125 u⁶ w³
+
+#this can't be simplified further because there are no like terms or common factors
+
+
+'''
+finding the degree and leading coefficient of a polynomial
+'''
+(6)(x)³ + (20)(x)⁸ + (-2) + (20)(x) =
+
+#arrange the terms so the variables with exponents go from left to right in descending order
+(20)(x)⁸ + (6)(x)³ + (20)(x) + (-2) =
+
+#the leading coefficient is the first term in the polynomial when arranged in standard form
+(20)
+
+#the degree is the power/exponent of the leading coefficient
+(8)
+
+
+'''
+finding the degree and leading coefficient of a polynomial
+'''
+(-4)(w)⁷ + (20)(w)⁹ + (-w) + (-20)(w)⁸
+
+#arrange the terms so the variables with exponents go from left to right in descending order
+(20)(w)⁹ + (-20)(w)⁸ + (-4)(w)⁷ + (-w)
+
+#the leading coefficient is the first term in the polynomial when arranged in standard form
+(20)
+
+#the degree is the power/exponent of the leading coefficient
+(9)
